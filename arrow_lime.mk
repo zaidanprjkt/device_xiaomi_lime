d9c912223ce64d3a0d68a2605ea599e947b52957
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-02-21 20:13:19 +0700

    lime: no gapps

--- arrow_lime.mk
+++ arrow_lime.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/lime/device.mk)
@@ -19,5 +19,8 @@ PRODUCT_DEVICE := lime
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2010J19SG
+
+WITH_GAPPS=false
+WITH_GMS=false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
