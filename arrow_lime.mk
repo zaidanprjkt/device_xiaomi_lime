d6d3e90e836ca582ad185cb238b82b7352834b08
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-04-26 00:15:39 +0700

    ProtonPlus-ify (1/2)

--- arrow_lime.mk
+++ arrow_lime.mk
@@ -8,13 +8,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
-
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/lime/device.mk)
 
-PRODUCT_NAME := arrow_lime
+PRODUCT_NAME := lime
 PRODUCT_DEVICE := lime
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
