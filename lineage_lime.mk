f92866cb4ee994228a7fec309e1f6a94e878296d
zaidannn7 <scaledzdn@gmail.com>
2024-05-15 13:44:34 +0700

    RisingOS-ify!
    Signed-off-by: zaidannn7 <scaledzdn@gmail.com>

--- lineage_lime.mk
+++ lineage_lime.mk
@@ -9,10 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Enable AOSP recovery flashing
-TARGET_USES_AOSP_RECOVERY := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -21,7 +18,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, device/xiaomi/lime/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lime
+PRODUCT_NAME := lineage_lime
 PRODUCT_DEVICE := lime
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -30,3 +27,14 @@ PRODUCT_MODEL := Redmi 9T
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/lime_global/lime:12/SKQ1.211202.001/V13.0.3.0.SJQMIXM:user/release-keys
+
+# Rising flags
+RISING_CHIPSET="Snapdragon 662"
+RISING_MAINTAINER="scaledzdn"
+TARGET_ENABLE_BLUR := false
+WITH_GMS := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 662" \
+    RISING_MAINTAINER="scaledzdn"
+    
